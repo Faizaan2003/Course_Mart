4a8b2c8109cdc5d6ccb699abdc41eea827b88d4a
Faizaan Shaik P <faizaanshaik2002@gmail.com>
2024-08-17 23:34:48 +0530

    7th commit

--- server/index.js
+++ server/index.js
@@ -28,4 +28,4 @@ mongoose.connect(uri, {
   dbName: "courses",
 });
 
-// app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log("Server running on port 3000"));
